3a6f7190501063cad5a1127b5a4d360cd8ed0477
Leyenddx <jorgeluigi400@gmail.com>
2026-02-09 08:41:39 -0700

    [AGREGA] Agregamos un proyecto vacio de XCode - MAC

---
+++ prueba01_hola_mundo/prueba01_hola_mundo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  prueba01_hola_mundo
+//
+//  Created by alumno on 2/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
